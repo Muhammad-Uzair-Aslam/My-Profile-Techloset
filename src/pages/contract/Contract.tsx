3f79c65f638666ae981b59cc4128ab1a9b96c7c1
Uzair Aslam <uzairaslam990@gmail.com>
2025-03-19 11:09:41 +0500

    contract page

---
+++ src/pages/contract/Contract.tsx
@@ -0,0 +1,70 @@
+
+export default function Contract() {
+  return (
+    <div className="mx-auto py-5 px-1 bg-white">
+      <h1 className="text-2xl font-bold text-gray-800 text-center mb-4">BOOTCAMPER AGREEMENT</h1>
+      <p className="mb-4 text-gray-700">
+        This agreement was made as of 17/12/2024 between Techloset Solutions (PVT) LTD. A corporation incorporated under the laws of Pakistan, and having its principal 
+        place of business at Amin Town, West Canal road, Faisalabad (the "Employer"); and Muhammad Uzair Aslam , (the "Bootcamper"). Whereas the employer desires to 
+        obtain the benefit of the services of the employee, and the bootcamper desires to render such services on the terms and conditions set forth.
+      </p>
+      <p className="mb-4 text-gray-700">
+        In consideration of the promises and other good and valuable consideration (the sufficiency and receipt of which are hereby acknowledged) the parties agree as 
+        follows:
+      </p>
+      <h2 className="text-xl font-bold text-gray-800 mt-6 mb-3">TERMS & CONDITIONS</h2>
+      <p className="mb-4 text-gray-700">
+        It is not permissible to work on freelancing platforms while on duty. Using Fiverr, Upwork, freelancer.com or any other such site during office hours is prohibited. 
+        Collaborating with team members to work on personal projects is forbidden.
+      </p>
+      <h2 className="text-xl font-bold mt-6 text-gray-800 mb-3">WORKING HOURS</h2>
+      <p className="mb-4 text-gray-700">
+        Working hours are based on the normal operating hours which are from 10:00 am -7:00 pm, for onsite and flexible for remote candidates between 10:00 am till 11:59 
+        PM. Six days a week Monday to Saturday. The time from 2 pm to 3 pm is taken as a lunch break. Candidates are required to spend minimum 8 hours daily.
+      </p> 
+      <h2 className="text-xl font-bold mt-6 text-gray-800 mb-3">TERMINATION</h2>
+      <p className="mb-4 text-gray-700">
+        A minimum of 1 months advance notice period is mandatory in case of dropping Bootcamp. Termination from company will depend on various reason like 
+        attendance shortage and performance. Terminated and dropped candidates will not be awarded certificates.
+      </p>   
+      <h2 className="text-xl font-bold mt-6 text-gray-800 mb-3">CONFIDENTIALITY</h2>
+      <p className="mb-4 text-gray-700">
+        During the course of your bootcamp with Techloset Solutions (PVT) LTD., you will be entrusted with confidential and proprietary information. You agree that such 
+        information will not be released or divulged, whether directly or indirectly, unless authorized by company policy, required by law, or through the express written 
+        consent of the Techloset Solutions (PVT) LTD. Given under the hand of the proper officer with authority to give such consent.
+      </p>
+      <h2 className="text-xl font-bold mt-6 text-gray-800 mb-3">PROJECTS PROPERLY</h2>
+      <p className="mb-4 text-gray-700">
+      During the period of your bootcamp you will work on different projects that are entitled to be Techloset Solution’s property. You agree that you shall not use any of those project links as an individual development portfolio. If this condition is breached, Strict actions legal action falling under the intellectual property law will be taken against you.
+      </p>
+      <h2 className="text-xl font-bold mt-6 text-gray-800 mb-3">Last Update</h2>
+      <p className="mb-4 text-gray-700">
+      07/02/2023      
+      </p>
+      <h2 className="text-xl font-bold mt-6 mb-3">DECLARATION</h2>
+      <p className="mb-4 text-gray-700">
+        Mr. Muhammad Uzair Aslam hereby acknowledges and agrees that I would serve Techloset Solutions (PVT) LTD. For a period of 6 months from the date of joining. I 
+        also agree, failing to which will result in no certification as a penalty as well as no experience documents (release letter, experience certificate, etc.) will be issued to 
+        me.
+      </p>
+      <p className="mb-4 text-gray-700">
+        I also divulge that I will be completely responsible for bearing the consequences or legal action (if required) in case of breaching any of the above-mentioned terms 
+        of services by Techloset Solution (PVT) LTD.
+      </p>
+      <p className="mb-4 text-gray-700">
+        I, Mr. Muhammad Uzair Aslam hereby also acknowledge that my bootcamp contract will be renewed if offered job.
+      </p>
+      <p className="mb-4 text-gray-700">
+        I accept the terms & conditions imposed by Techloset Solutions (PVT) LTD.
+      </p>
+      <h2 className="text-xl font-bold mt-6 mb-3">SIGNATURE:</h2>
+      <div className="mb-6 p-6 bg-gray-100 inline-block">
+        <img 
+          src="/api/placeholder/200/100" 
+          alt="Signature" 
+          className="w-40" 
+        />
+      </div>
+    </div>
+  );
+}
